2130e766c09914e2c6832fc80a31cea4699cdb26
pinomartinezruben <rpm@ku.edu>
2026-02-03 03:44:53 -0600

    Completed step 5 which allows me to raise an exception if route doesn't exist, and added new route

--- Backend/main.py
+++ Backend/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from track_data import track_instance
 
 app = FastAPI()
@@ -7,9 +7,8 @@ app = FastAPI()
 def root():
     return {"Hello": "World"}
 
-@app.get("/tracks")
-def get_tracks():
-    sample_track = track_instance(
+tracks = {
+    "1": track_instance(
         id="1",
         source="YouTube",
         source_id="abc123",
@@ -17,5 +16,27 @@ def get_tracks():
         artist_or_channel="Rick Astley",
         duration=213,
         external_url="https://youtu.be/dQw4w9WgXcQ?si=qDKboURQ8BaBU8eb"
+    ),
+    "2": track_instance(
+        id="2",
+        source="YouTube",
+        source_id="def456",
+        title="Mr. Bombastic",
+        artist_or_channel="Chuck Norris",
+        duration=67,
+        external_url="https://youtu.be/DWzL4VhRvZI?si=NVphcwiWi4HAVZRv"
     )
-    return [sample_track]
+}
+
+@app.get("/tracks/{track_id}")
+def get_tracks(track_id: str):
+    if track_id not in tracks:
+        raise HTTPException(status_code=404, detail="Track is not found")
+    return tracks[track_id]
+
+@app.get("/playlists")
+def get_playlists():
+    return [
+        {"id":"1","name":"Favorites","Track Count":"4"},
+        {"id": "2", "name": "Workout Mix","Track Count": "5"}
+    ]
